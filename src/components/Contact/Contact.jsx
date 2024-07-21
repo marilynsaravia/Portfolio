4a355f9835ef893f46787eff839ecee00b8faaf0
Marilyn Saravia <160709168+marilynsaravia@users.noreply.github.com>
2024-07-21 12:34:08 +0200

    Email correction

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:goitisaraviamarilyn@gmail.com">goitisaraviamarilyn@gmail.com</a>
+          <a href="mailto:goitiasaraviamarilyn@gmail.com">goitiasaraviamarilyn@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
